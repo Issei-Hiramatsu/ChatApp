f17b9e8cf22072fe4b77817e3eaffe903342c070
issei <issei.hiramatsu@gmail.com>
2022-10-23 22:38:06 +0900

    Firebaseログイン設定完了

--- flutter_chatapp/lib/main.dart
+++ flutter_chatapp/lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screen/chat_scrren.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
@@ -20,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ChatScreen(),
+      home: ChatScreen(),
     );
   }
 }
