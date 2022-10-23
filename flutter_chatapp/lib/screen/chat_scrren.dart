bb5897d5030689b88eed3cceff4ad04549d4a800
issei <issei.hiramatsu@gmail.com>
2022-10-23 21:21:22 +0900

    一時的にfirebasecoreの追加

--- flutter_chatapp/lib/screen/chat_scrren.dart
+++ flutter_chatapp/lib/screen/chat_scrren.dart
@@ -7,22 +7,29 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: ((ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text('Test'),
-            )),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/VIOLe3yq6kEhSaSaSwd6/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data?.docs;
+          return ListView.builder(
+            itemCount: streamSnapshot.data?.docs.length,
+            itemBuilder: ((ctx, index) => Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text(documents?[index]['text']),
+                )),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/VIOLe3yq6kEhSaSaSwd6/messages')
-              .snapshots()
-              .listen((data) {
-            print(data);
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
