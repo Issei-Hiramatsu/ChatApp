f404f5d3f149554dee93d59c7a9c8968184a3493
issei <issei.hiramatsu@gmail.com>
2022-10-23 23:00:52 +0900

    初期化エラー修復

--- flutter_chatapp/lib/main.dart
+++ flutter_chatapp/lib/main.dart
@@ -5,10 +5,12 @@ import 'firebase_options.dart';
 import 'screen/chat_scrren.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  } catch (e) {
+    print('Have error');
+  }
   runApp(MyApp());
 }
 
