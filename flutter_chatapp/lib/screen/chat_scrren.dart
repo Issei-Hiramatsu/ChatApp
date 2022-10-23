6f8e07979bcfdd427546940da278eca73714ce6a
issei <issei.hiramatsu@gmail.com>
2022-10-23 14:09:33 +0900

    xcodeのビルドを軽くする

--- flutter_chatapp/lib/screen/chat_scrren.dart
+++ flutter_chatapp/lib/screen/chat_scrren.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -11,6 +12,17 @@ class ChatScreen extends StatelessWidget {
               padding: EdgeInsets.all(8),
               child: Text('Test'),
             )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/VIOLe3yq6kEhSaSaSwd6/messages')
+              .snapshots()
+              .listen((data) {
+            print(data);
+          });
+        },
       ),
     );
   }
