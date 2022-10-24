bb40d1e42ef079322f48df9afa2374cc2873b965
issei <issei.hiramatsu@gmail.com>
2022-10-24 16:34:15 +0900

    ログアウト機能の実装

--- flutter_chatapp/lib/main.dart
+++ flutter_chatapp/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chatapp/screen/chat_scrren.dart';
 
 import 'screen/auth_screen.dart';
 
@@ -20,30 +22,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterChat',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSwatch(
-      //     primarySwatch: Colors.pink,
-      //   ).copyWith(
-      //     secondary: Colors.purple,
-      //     brightness: Brightness.dark,
-      //   ),
-      //   backgroundColor: Colors.pink,
-      //   buttonTheme: ButtonTheme.of(context).copyWith(
-      //     buttonColor: Colors.pink,
-      //     textTheme: ButtonTextTheme.primary,
-      //     shape: RoundedRectangleBorder(
-      //       borderRadius: BorderRadius.circular(20),
-      //     ),
-      //   ),
-      // ),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: Colors.green,
+        title: 'FlutterChat',
+        // theme: ThemeData(
+        //   colorScheme: ColorScheme.fromSwatch(
+        //     primarySwatch: Colors.pink,
+        //   ).copyWith(
+        //     secondary: Colors.purple,
+        //     brightness: Brightness.dark,
+        //   ),
+        //   backgroundColor: Colors.pink,
+        //   buttonTheme: ButtonTheme.of(context).copyWith(
+        //     buttonColor: Colors.pink,
+        //     textTheme: ButtonTextTheme.primary,
+        //     shape: RoundedRectangleBorder(
+        //       borderRadius: BorderRadius.circular(20),
+        //     ),
+        //   ),
+        // ),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            secondary: Colors.green,
+          ),
+          textTheme:
+              const TextTheme(bodyText2: TextStyle(color: Colors.purple)),
         ),
-        textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.purple)),
-      ),
-      home: AuthScreen(),
-    );
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: ((context, userSnapshot) {
+            if (userSnapshot.hasData) {
+              return ChatScreen();
+            }
+            return AuthScreen();
+          }),
+        ));
   }
 }
