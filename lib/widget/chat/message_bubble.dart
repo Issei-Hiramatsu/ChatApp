0db6a4bb7bbc9e9241baa51ca600d469f1437b77
issei <issei.hiramatsu@gmail.com>
2022-11-04 16:35:30 +0900

    fixed Bad state

--- lib/widget/chat/message_bubble.dart
+++ lib/widget/chat/message_bubble.dart
@@ -17,59 +17,67 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
+      //clipBehavior: Clip.none,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe
-                ? Colors.grey[300]
-                : Theme.of(context).colorScheme.secondary,
-            borderRadius: BorderRadius.only(
-              topLeft: const Radius.circular(12),
-              topRight: const Radius.circular(12),
-              bottomLeft:
-                  !isMe ? const Radius.circular(0) : const Radius.circular(12),
-              bottomRight:
-                  isMe ? const Radius.circular(0) : const Radius.circular(12),
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe
+                    ? Colors.grey[300]
+                    : Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: !isMe
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                  bottomRight: isMe
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                ),
+              ),
+              width: 140,
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: const EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 8,
+              ),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    userName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isMe ? Colors.black : Colors.white,
+                    ),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isMe ? Colors.black : Colors.white,
+                    ),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
             ),
-          ),
-          width: 140,
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          margin: const EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 8,
-          ),
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                userName,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: isMe ? Colors.black : Colors.white,
-                ),
-              ),
-              Text(
-                message,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: isMe ? Colors.black : Colors.white,
-                ),
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
-              ),
-            ],
-          ),
+          ],
         ),
-        // const Positioned(
-        //   top: 0,
-        //   right: 0,
-        //   child: CircleAvatar(),
-        // ),
+        Positioned(
+          top: -30,
+          left: 140,
+          child: CircleAvatar(),
+        ),
       ],
     );
   }
