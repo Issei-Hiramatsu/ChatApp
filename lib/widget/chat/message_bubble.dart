f072280e1b242fa043abcd0432272859f5165a61
issei <issei.hiramatsu@gmail.com>
2022-11-04 22:32:28 +0900

    通知パッケージのインストール

--- lib/widget/chat/message_bubble.dart
+++ lib/widget/chat/message_bubble.dart
@@ -75,7 +75,7 @@ class MessageBubble extends StatelessWidget {
         ),
         //ゆくゆくはLineみたいなUIに変更する
         Positioned(
-          top: -10,
+          top: 0,
           left: isMe ? null : 120,
           right: isMe ? 120 : null,
           child: CircleAvatar(backgroundImage: NetworkImage(userImage)),
